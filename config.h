552a4455ad9c1c54e4134f73942c1dbc9af69cb4
code399 <code399@163.com>
2023-08-03 16:12:36 +0800

    Update

--- config.h
+++ config.h
@@ -1,26 +1,24 @@
 #include "X11/XF86keysym.h"
 #include "gaplessgrid.c"
-/* See LICENSE file for copyright and license details. */
 
-/* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 4;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray             = 1;   /* 0 means no systray */
+static const unsigned int borderpx        = 1;        /* 窗口边框大小 */
+static const unsigned int gappx           = 4;        /* 窗口间距 */
+static const unsigned int snap            = 32;       /* snap pixel */
+static const int showbar                  = 1;        /* 是否显示状态栏 */
+static const int topbar                   = 1;        /* 状态栏显示位置 0 底部 1 顶部 */
+static const unsigned int systraypinning  = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing  = 2;   /* systray spacing */
+static const int systraypinningfailfirst  = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray              = 1;   /* 是否显示系统托盘 */
 
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=medium:size=13",
-					"WenQuanYi Micro Hei:style=Regular:size=13:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=13";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]                = { "JetBrainsMono Nerd Font:style=medium:size=13",
+					      "WenQuanYi Micro Hei:style=Regular:size=13:antialias=true:autohint=true" };
+static const char dmenufont[]             = "JetBrainsMono Nerd Font:size=13";
+static const char col_gray1[]             = "#222222";
+static const char col_gray2[]             = "#444444";
+static const char col_gray3[]             = "#bbbbbb";
+static const char col_gray4[]             = "#eeeeee";
+static const char col_cyan[]              = "#005577";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -43,7 +41,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
